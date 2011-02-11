9ce5960f44371b34574aafe8a44d1f0013efd940
Tim <tim@rtlcores.com>
2011-02-11 15:39:40 -0800

    Forgot to add the help.py file

---
+++ help.py
@@ -0,0 +1,7 @@
+missing_logfile_help = """
+    When a simulation runs it will generate a log file that contains all
+    of the output that it prints. The ScoreBoard module attempts to read this
+    file and generate a tally of the tests performance.
+    If that log file cannot be found it probably means the design failed to
+    compile for some reason and therefore the simulation never ran.
+"""
