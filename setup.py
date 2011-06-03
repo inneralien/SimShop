63f1e68d601ce76c5b425f838c3f898254ae05ce
Tim <tim@rtlcores.com>
2011-06-03 15:12:57 -0700

    Added classifiers and py2exe support.

--- setup.py
+++ setup.py
@@ -16,8 +16,23 @@ options = {
     'license':'LICENSE.txt',
     'description':'Easy Verilog simulation',
     'long_description':open('README.txt').read(),
+    'classifiers' : [
+          'Development Status :: 3 - Alpha',
+          'Environment :: Console',
+          'Intended Audience :: End Users/Desktop',
+          'Intended Audience :: Educators',
+          'License :: OSI Approved :: BSD License',
+          'Operating System :: MacOS :: MacOS X',
+          'Operating System :: Microsoft :: Windows',
+          'Operating System :: POSIX',
+          'Programming Language :: Python',
+          'Topic :: Scientific/Engineering',
+          'Topic :: Scientific/Engineering :: Electronic Design Automation (EDA)',
+          'Topic :: Utilities',
+          ],
 }
 
+# Mac specific for py2app
 if len(sys.argv) >= 2 and sys.argv[1] == 'py2app':
     try:
         import py2app
@@ -31,6 +46,15 @@ if len(sys.argv) >= 2 and sys.argv[1] == 'py2app':
             'argv_emulation': True
         }
     }
+
+# Windows specific for py2exe
+if len(sys.argv) >= 2 and sys.argv[1] == 'py2exe':
+    try:
+        import py2exe
+    except ImportError:
+        print 'Could not import py2exe.  Windows bundle could not be built.'
+        sys.exit(0)
+    options['console'] = ['bin/shop.py']
 
 # run the setup
 setup(**options)
